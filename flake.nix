87d9f5208bd4ec2de73e7b80912bd79a97d03867
petitstrawberry <petitstrawberry.dev@gmail.com>
2026-07-20 12:05:55 +0900

    add macvdmtool support in flake.nix and update .gitignore

--- flake.nix
+++ flake.nix
@@ -15,6 +15,10 @@
       url = "github:petitstrawberry/scarlet-sdk";
       flake = false;
     };
+    macvdmtool-src = {
+      url = "github:AsahiLinux/macvdmtool/b22ae51eb43a0e1daa21d41616ac899f28e7bf8a";
+      flake = false;
+    };
   };
 
   outputs =
@@ -23,6 +27,7 @@
       nixpkgs,
       scarlet-rust-toolchain,
       scarlet-sdk,
+      macvdmtool-src,
     }:
     let
       supportedSystems = [
@@ -39,6 +44,10 @@
         let
           pkgs = import nixpkgs { inherit system; };
           rustToolchain = scarlet-rust-toolchain.packages.${system}.scarlet-rust-toolchain;
+          pythonEnv = pkgs.python3.withPackages (ps: [
+            ps.construct
+            ps.pyserial
+          ]);
 
           cargo-scarlet = pkgs.rustPlatform.buildRustPackage {
             pname = "cargo-scarlet";
@@ -56,6 +65,28 @@
             cargoLock.lockFile = "${scarlet-sdk}/Cargo.lock";
           };
 
+          macvdmtool = pkgs.stdenv.mkDerivation {
+            pname = "macvdmtool";
+            version = "unstable-b22ae51";
+            src = macvdmtool-src;
+
+            nativeBuildInputs = [ pkgs.gnumake ];
+
+            installPhase = ''
+              runHook preInstall
+              install -Dm755 macvdmtool "$out/bin/macvdmtool"
+              runHook postInstall
+            '';
+
+            meta = {
+              description = "Apple Silicon Virtualization.framework companion tool";
+              homepage = "https://github.com/AsahiLinux/macvdmtool";
+              license = pkgs.lib.licenses.asl20;
+              mainProgram = "macvdmtool";
+              platforms = [ "aarch64-darwin" ];
+            };
+          };
+
         in
         {
           default = pkgs.mkShell {
@@ -66,11 +97,25 @@
               pkgs.git
               pkgs.curl
               pkgs.gnumake
-            ];
+              pkgs.perl
+              pkgs.dtc
+              pkgs.picocom
+              pkgs.rsync
+              pkgs.tmux
+              pkgs.llvmPackages.llvm
+              pkgs.clang
+              pkgs.lld
+              pkgs.pkgsCross.aarch64-multiplatform.buildPackages.gcc
+              pythonEnv
+            ]
+            ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isDarwin [ macvdmtool ];
             hardeningDisable = [ "zerocallusedregs" ];
             shellHook = ''
               export PATH="${rustToolchain}/bin:$PATH"
               export SCARLET_RUST_ACTIVE_BIN="${rustToolchain}/bin"
+              export CC_aarch64_unknown_scarlet=aarch64-unknown-linux-gnu-gcc
+              export AR_aarch64_unknown_scarlet=aarch64-unknown-linux-gnu-ar
+              export RANLIB_aarch64_unknown_scarlet=aarch64-unknown-linux-gnu-ranlib
             '';
           };
         }
